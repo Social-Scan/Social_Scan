41f4d25bdee5c9a3ce023e3e29ad06abf2f81472
Social-Scan <socialscanamazon@gmail.com>
2024-11-25 22:23:48 +0530

    Delete Backend directory

--- Backend/admin.py
+++
@@ -1,9 +0,0 @@
-# matcher/admin.py
-from django.contrib import admin
-from .models import Product
-
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'amazon_link')  # Fields to display in the admin list view
-    search_fields = ('name', 'description')  # Allow search by name and description
-
-admin.site.register(Product, ProductAdmin)
